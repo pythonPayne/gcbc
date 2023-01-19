5dec365349b756226c0994682cd9b9287c8399c5
Stephen Payne <stephen.thomas.payne@gmail.com>
2023-01-19 07:13:12 -0600

    fix about page sizing

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -54,7 +54,7 @@ const About = ({data}) => {
     const edge = gcbcs.find(edge => edge.node.title === title)    
     const image = <GatsbyImage className={`shadow-xl h-full rounded md:rounded-lg ${order1} md:order-none`} image={edge.node.pic.asset['gatsbyImageData']} alt={title} />
     const words = <div className={`flex flex-col ${order2} md:order-none pb-28 md:pb-0`}>
-                    <div className={`text-4xl font-bold py-6 text-gray-600`}>{edge.node.title}</div>
+                    <div className={`text-3xl md:text-4xl font-semibold py-6 text-gray-600`}>{edge.node.title}</div>
                     <div className={`leading-[1.8rem]`}>{edge.node.displayText}</div>
                   </div>
 
@@ -73,7 +73,7 @@ const About = ({data}) => {
       <>
         <GatsbyImage className={`shadow-xl h-full rounded-lg`} image={elder.node.pic.asset['gatsbyImageData']} alt={elder.node.title} />
         <div className={`flex flex-col pb-32 md:pb-0`}>
-          <div className={`text-4xl font-bold py-6 text-gray-600`}>{elder.node.title}</div>
+          <div className={`text-2xl font-semibold py-6 text-gray-600`}>{elder.node.title}</div>
           <div className={`leading-[1.8rem]`}>{elder.node.displayText}</div>
         </div>        
       </>
@@ -87,11 +87,16 @@ const About = ({data}) => {
 
       <div className={`max-w-[1200px]`}>
       
-        <div className={`px-12 pt-12 pb-36 min-h-screen flex flex-col justify-center bg-gray-100 transition-all ${showMenu ? "blur-sm duration-500" : "blur-none duration-200"}`}>
+        <div className={`px-6 md:px-12 pt-12 pb-36 min-h-screen flex flex-col justify-center bg-gray-100 transition-all ${showMenu ? "blur-sm duration-500" : "blur-none duration-200"}`}>
           
           {/* G C B C */}
-          <div className={`py-4 mb-12 md:mb-16 flex justify-center text-[#09314C] text-5xl md:text-6xl font-sans font-bold 
-          tracking-widest border-b-2`}>G &nbsp;C &nbsp;B &nbsp;C</div>
+          <div className={`flex flex-col items-center`}>
+            <div className={`py-2 flex justify-center text-[#09314C] 
+            text-4xl md:text-6xl font-sans font-bold tracking-widest`}>G &nbsp;C &nbsp;B &nbsp;C</div>
+            <div className={`font-serif text-gray-500`}>what's in a name?</div>
+          </div>
+
+          <div className={`border-b-2 my-6 md:my-8`}></div>
 
           <div className={`flex flex-col md:grid md:grid-cols-2 md:gap-x-16 md:gap-y-24`}>
 
@@ -109,8 +114,13 @@ const About = ({data}) => {
           <div className={`py-6 md:py-12`}></div>          
 
           {/* OUR ELDERS */}
-          <div className={`py-4 mb-12 md:mb-16 flex justify-center text-[#09314C] text-5xl md:text-6xl font-sans font-bold 
-          tracking-widest border-b-2`}>Our Elders</div>
+          <div className={`flex flex-col items-center`}>
+            <div className={`py-2 flex justify-center text-[#09314C] 
+            text-4xl md:text-6xl font-sans font-bold tracking-widest`}>Our Elders</div>
+            <div className={`font-serif text-gray-500`}></div>
+          </div>
+
+          <div className={`border-b-2 my-6 md:my-8`}></div>
   
           <div className={`flex flex-col md:grid md:grid-cols-2 md:gap-x-16 md:gap-y-32`}>
             {elders.map(elder => elderCard(elder))}
