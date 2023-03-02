e19224b3d1ce4a5dc9870a795e6914c6c99e3f1a
Stephen Payne <stephen.thomas.payne@gmail.com>
2023-03-02 10:54:47 -0600

    add site icon

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -4,23 +4,36 @@
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
   siteMetadata: {},
   plugins: [
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         overlayDrafts: false,
         watchMode: false,
-        watchModeBuffer: 150
-      }
-    },    
+        watchModeBuffer: 150,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#09314C`,
+        display: `standalone`,
+        icon: `src/images/Emblem_Blue.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,        
-  ]
+    `gatsby-transformer-sharp`,
+  ],
 };
