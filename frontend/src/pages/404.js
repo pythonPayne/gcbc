9a4a5ae97d4c050f48ea60ee8f96dfd3e225faf9
Stephen Payne <stephen.thomas.payne@gmail.com>
2023-04-13 20:27:01 -0500

    update 404 page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleShowMenu } from '../redux/actions/layout'
-import Layout from '../components/Layout'
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { toggleShowMenu } from "../redux/actions/layout"
+import Layout from "../components/Layout"
 
 const NotFoundPage = () => {
   const dispatch = useDispatch()
@@ -11,7 +11,18 @@ const NotFoundPage = () => {
 
   return (
     <Layout>
-      <div page={'notfound'} className={`mt-20 min-h-screen`}>      
+      <div
+        page={"notfound"}
+        className={`min-h-screen bg-red-100 flex w-full justify-center items-center`}
+      >
+        <div className={`pt-20 text-center text-md md:text-lg`}>
+          <p>
+            We're transitioning the Sunday School data, older sermon files, and
+            some other content to this new site.
+          </p>
+          <br />
+          <p>They should all be online shortly.</p>
+        </div>
       </div>
     </Layout>
   )
