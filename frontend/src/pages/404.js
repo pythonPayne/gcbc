c61b78b71805c6cc62c7c8e5d17e500319b68f5e
Stephen Payne <stephen.thomas.payne@gmail.com>
2023-09-16 12:54:24 -0500

    404 update

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -16,12 +16,8 @@ const NotFoundPage = () => {
         className={`min-h-screen bg-red-100 flex w-full justify-center items-center`}
       >
         <div className={`pt-20 text-center text-md md:text-lg`}>
-          <p>
-            We're transitioning the Sunday School data, older sermon files, and
-            some other content to this new site.
-          </p>
+          <p>Page not found.</p>
           <br />
-          <p>They should all be online shortly.</p>
         </div>
       </div>
     </Layout>
